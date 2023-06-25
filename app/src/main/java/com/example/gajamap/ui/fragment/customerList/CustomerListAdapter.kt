15e2140077aff83bfbb84f7af87f0f0f385235a6
cindy <chaewon1769@daum.net>
2023-06-25 09:28:28 +0900

    :sparkles: feature: 고객 리스트 페이지 layout

---
+++ app/src/main/java/com/example/gajamap/ui/fragment/customerList/CustomerListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.gajamap.ui.fragment.customerList
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.gajamap.databinding.ItemListBinding
+
+class CustomerListAdapter(private val dataList : ArrayList<Customer>): RecyclerView.Adapter<CustomerListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(private val binding: ItemListBinding):
+            RecyclerView.ViewHolder(binding.root){
+                fun bind(data: Customer){
+                    Glide.with(binding.itemProfileImg.context)
+                        .load(data.img)
+                        .into(binding.itemProfileImg)
+                    binding.itemProfileAddressDetail.text = data.address
+                    binding.itemProfileName.text = data.name
+                    binding.itemProfilePhoneDetail.text = data.phone
+                    binding.itemProfileDistance.text = data.distance
+
+                }
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataList[position])
+
+        holder.itemView.setOnClickListener{
+            itemClickListener.onClick(it, position)
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener){
+        this.itemClickListener = onItemClickListener
+    }
+
+    private lateinit var itemClickListener : OnItemClickListener
+}
---
+++ app/src/main/java/com/example/gajamap/viewmodel/ListViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.gajamap.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ListViewModel(private val tmp: String): ViewModel() {
+
+    class ListViewModelFactory(private val tmp: String)
+        : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            // modelClass에 MainViewModel이 상속되었는지 확인
+            if (modelClass.isAssignableFrom(ListViewModel::class.java)) {
+                // 맞다면 MainViewModel의 파라미터 값을 넘겨줌
+                return ListViewModel(tmp) as T
+            }
+            // 상속이 되지 않았다면 IllegalArgumentException을 통해 상속이 되지 않았다는 에러를 띄움
+            throw IllegalArgumentException("Not found ViewModel class.")
+        }
+    }
+
+}
